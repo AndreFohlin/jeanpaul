3b2929b23cf2e670d707e5287df1df5956106d65
Martin Johansson <martinj83@gmail.com>
2017-10-30 16:04:19 +0100

    Put reactions in a different file.

---
+++ reactions.js
@@ -0,0 +1,17 @@
+exports.getReactions = function() {
+	return [
+		'Meow', 
+		'Mjaeowo', 
+		'..............', 
+		'Mjawwarw', 
+		'Rrrrrrrrrrrr', 
+		'*Attacks random body part*', 
+		':jpa:', 
+		':scream_cat:', 
+		':doge::gun:', 
+		':cat:', 
+		':cat2:', 
+		':kan:'
+	];
+};
+
