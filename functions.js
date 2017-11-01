b5bb18c30d16c808dec320ff446ffec48027155d
Martin Johansson <martin.johansson@bannerflow.com>
2017-11-01 12:52:23 +0100

    Added a few more functions to separate concerns.

--- functions.js
+++ functions.js
@@ -1,33 +1,19 @@
 let moment = require('moment');
+let request = require('request');
+let jpFunctions = require('./functions.js'); // Jepp, jag kan nog inte tillräckligt mycket om node. Men denna importerar sig själv :D
 
-exports.getReactions = function() {
-	return [
-		'Meow', 
-		'Mjaeowo', 
-		'..............', 
-		'Mjawwarw', 
-		'Rrrrrrrrrrrr', 
-		'* Attacks random body part *', 
-		':jpa:', 
-		':scream_cat:', 
-		':doge::gun:', 
-		':cat:', 
-		':cat2:', 
-		':kan:',
-        'mjao?',
-        'MJAO!',
-        'Mjaoooo :heart_eyes_cat:',
-        ':pouting_cat: :fish:'
-	];
-};
+let postedFridayFrog = false;
+
+let lastBitcoinPrice = 0;
+let lastBitcoinPriceCheck = moment();
 
 exports.configureMoment = function() {
     moment.updateLocale('en', {
         relativeTime : {
             future: "om %s",
-            past:   "%s sedan",
-            s  : 'ett par sekunder',
-            ss : '%d sekunder',
+            past: "%s sedan",
+            s: 'ett par sekunder',
+            ss: '%d sekunder',
             m:  "en minut",
             mm: "%d minuter",
             h:  "en timme",
@@ -42,6 +28,33 @@ exports.configureMoment = function() {
     });
 }
 
+exports.getReactions = function() {
+	return [
+		'Meow',
+		'Mjaeowo',
+		'..............',
+		'Mjawwarw',
+		'Rrrrrrrrrrrr',
+		'* Attacks random body part *',
+		':jpa:',
+		':scream_cat:',
+		':doge::gun:',
+		':cat:',
+		':cat2:',
+		':kan:',
+        'mjao?',
+        'MJAO!',
+        'Mjaoooo :heart_eyes_cat:',
+        ':pouting_cat: :fish:'
+	];
+};
+
+exports.meow = function(rtm, channelId) {
+    let meows = jpFunctions.getReactions();
+    let meow = meows[Math.floor(Math.random()*meows.length)];
+    rtm.sendMessage(meow, channelId);
+}
+
 // Lägg till tusen-separatorer, så det blir 160 000 istället för 160000.
 exports.numberParser = function(nummer) {
     var nummerRegex = /(\d+)(\d{3})/;
@@ -51,4 +64,53 @@ exports.numberParser = function(nummer) {
         }
         return w;
     });
+}
+
+exports.getBitcoinPrice = function(event, rtm) {
+    request('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=SEK&apikey=ZBFWZJJKL5WA9XD0', (error, response, sekBody) => {
+        if (response.statusCode === 200) {
+            let bitcoinSEK = JSON.parse(sekBody)['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+            
+            request('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=ZBFWZJJKL5WA9XD0', (error, response, usdBody) => {
+                if (response.statusCode === 200) {
+                    let bitcoinUSD = JSON.parse(usdBody)['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+
+                    // Visar även den procentuella ändringen, utifall JP har kollat kurserna tidigare.
+                    if (lastBitcoinPrice) {
+                        let bitcoinDifference = bitcoinUSD / lastBitcoinPrice - 1;
+                        let bitcoinTimePassed = lastBitcoinPriceCheck.fromNow();
+                        rtm.sendMessage(`Bitcoin har ändrats med *${(bitcoinDifference * 100).toFixed(2)}%* sedan jag kollade för ${bitcoinTimePassed} och kostar nu *${jpFunctions.numberParser(parseFloat(bitcoinSEK).toFixed(0))} kr*, eller *$${jpFunctions.numberParser(parseFloat(bitcoinUSD).toFixed(2))}* om man är en Amerikatt.`, event.channel);
+                    }
+                    else {
+                        rtm.sendMessage(`!!Bitcoin kostar just nu *${jpFunctions.numberParser(parseFloat(bitcoinSEK).toFixed(0))} kr*, eller *$${jpFunctions.numberParser(parseFloat(bitcoinUSD).toFixed(2))}* om man är en Amerikatt. Ehm... mjao.`, event.channel);
+                    }
+
+                    lastBitcoinPrice = bitcoinUSD;
+                    bitcoinTimePassed = moment();
+                }
+                else {
+                    rtm.sendMessage('Det gick inte att hämta bitcoin-kurserna... Meow :(');
+                }
+
+            });
+        }
+        else {
+            rtm.sendMessage('Det gick inte att hämta bitcoin-kurserna... Meow :(');
+        }
+    });
+}
+
+exports.postFrog = function(rtm, generalChannelId) {
+    // Posta fredagsgrodan, exakt klockan 08:07 på fredagar
+    if (!postedFridayFrog) {
+        if (moment().format('dddd HH:mm') === 'Friday 08:07') { 
+            rtm.sendMessage('https://i.imgur.com/ORDvwi9.jpg', generalChannelId);
+            postedFridayFrog = true;
+        }
+    }
+    else {
+        if (moment().format('dddd') !== 'Friday') {
+            postedFridayFrog = false; // Reset fridayfrog
+        }
+    }
 }
--- jeanpaul.js
+++ jeanpaul.js
@@ -1,7 +1,6 @@
 let RtmClient = require('@slack/client').RtmClient;
 let CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 let moment = require('moment');
-let request = require('request');
 
 let botConfig = require('./config.js');
 let jpFunctions = require('./functions.js');
@@ -19,10 +18,6 @@ let channel;
 let generalChannelId = 'C4RUQDECW'; // ID:t för #general
 let jpUtvecklingChannelId = 'C7RGH9LN5';
 
-let postedFridayFrog = false;
-let lastBitcoinPrice = 0;
-let lastBitcoinPriceCheck = moment();
-
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for (const c of rtmStartData.channels) {
@@ -33,7 +28,7 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  rtm.sendMessage('Autodeployed! Eller, jag har i alla fall startats om. Mjao.', jpUtvecklingChannelId);
+//   rtm.sendMessage('Autodeployed! Eller, jag har i alla fall startats om. Mjao.', jpUtvecklingChannelId);
   console.log('channel opened', channel);
 });
 
@@ -44,42 +39,10 @@ rtm.on(CLIENT_EVENTS.RTM.RAW_MESSAGE, (event) => {
     if (event.type === 'message') {
         if (event.text && (event.text.includes(`<@${myUserKey}>`) || event.text.includes(`JP`) || event.text.includes(`jp`))) {
             // let targetUser = '<@' +message.user+ '>';
-            let meow = meows[Math.floor(Math.random()*meows.length)];
-            let msg = meow;
-            rtm.sendMessage(msg, event.channel);
+            jpFunctions.meow(rtm, event.channel);
         }
         else if (event.text && event.text.includes('bitcoin')) {
-            request('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=SEK&apikey=ZBFWZJJKL5WA9XD0', (error, response, sekBody) => {
-                if (response.statusCode === 200) {
-                    let bitcoinSEK = JSON.parse(sekBody)['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-                    
-                    request('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey=ZBFWZJJKL5WA9XD0', (error, response, usdBody) => {
-                        if (response.statusCode === 200) {
-                            let bitcoinUSD = JSON.parse(usdBody)['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-
-                            // Visar även den procentuella ändringen, utifall JP har kollat kurserna tidigare.
-                            if (lastBitcoinPrice) {
-                                let bitcoinDifference = bitcoinUSD / lastBitcoinPrice - 1;
-                                let bitcoinTimePassed = lastBitcoinPriceCheck.fromNow();
-                                rtm.sendMessage(`Bitcoin har ändrats med *${(bitcoinDifference * 100).toFixed(2)}%* sedan jag kollade för ${bitcoinTimePassed} och kostar nu *${jpFunctions.numberParser(parseFloat(bitcoinSEK).toFixed(0))} kr*, eller *$${jpFunctions.numberParser(parseFloat(bitcoinUSD).toFixed(2))}* om man är en Amerikatt.`, event.channel);
-                            }
-                            else {
-                                rtm.sendMessage(`Bitcoin kostar just nu *${jpFunctions.numberParser(parseFloat(bitcoinSEK).toFixed(0))} kr*, eller *$${jpFunctions.numberParser(parseFloat(bitcoinUSD).toFixed(2))}* om man är en Amerikatt. Ehm... mjao.`, event.channel);
-                            }
-
-                            lastBitcoinPrice = bitcoinUSD;
-                            bitcoinTimePassed = moment();
-                        }
-                        else {
-                            rtm.sendMessage('Det gick inte att hämta bitcoin-kurserna... Meow :(');
-                        }
-
-                    });
-                }
-                else {
-                    rtm.sendMessage('Det gick inte att hämta bitcoin-kurserna... Meow :(');
-                }
-            });
+            jpFunctions.getBitcoinPrice(event, rtm);
         }
     }
 
@@ -89,9 +52,7 @@ rtm.on(CLIENT_EVENTS.RTM.RAW_MESSAGE, (event) => {
         // Messa #general ifall någon går online, med ett random mjao
         if (event.presence === 'active' && event.user != myUserKey) {
             if (numberOfMeows > 5) {
-                let meow = meows[Math.floor(Math.random()*meows.length)];
-                let msg = meow;
-                rtm.sendMessage(msg, generalChannelId);
+                jpFunctions.meow(rtm, generalChannelId);
                 numberOfMeows = 0;
             }
             else {
@@ -102,19 +63,7 @@ rtm.on(CLIENT_EVENTS.RTM.RAW_MESSAGE, (event) => {
 
     // En slags ping/pong mellan server och bot. Kommer drygt varje sekund, men kan missas utifall andra event pågår.
     if (event.type === 'pong') {
-
-        // Posta fredagsgrodan, exakt klockan 08:07 på fredagar
-        if (!postedFridayFrog) {
-            if (moment().format('dddd HH:mm') === 'Friday 08:07') { 
-                rtm.sendMessage('https://i.imgur.com/ORDvwi9.jpg', generalChannelId);
-                postedFridayFrog = true;
-            }
-        }
-        else {
-            if (moment().format('dddd') !== 'Friday') {
-                postedFridayFrog = false; // Reset fridayfrog
-            }
-        }
+        jpFunctions.postFrog(rtm, generalChannelId);
     }
 });
 
